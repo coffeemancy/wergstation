00e74942279b87a3da5782638d1a298a08a28c71
Carlton Stedman <sageman@gmail.com>
2014-09-21 12:22:11 -0400

    download source code pro font and install

--- recipes/xfce.rb
+++ recipes/xfce.rb
@@ -12,6 +12,10 @@ ns = node["wergstation"]
 # user's id, group, home
 id, grp, home = %w{ id group home }.map { |k| ns["user"][k] }
 
+# ~/.fonts dir
+dotfonts = ::File.join(home, ".fonts")
+
+# config files for xfce4
 cfgs = %w{ helpers.rc
            terminal/terminalrc }.
   map { |cfg| ".config/xfce4/#{cfg}" }
@@ -39,4 +43,50 @@ cfgs.each do |cfg|
     variables(:font => "Source Code Pro Medium 12")
     action :create
   end
+end
+
+## Install unzip
+#
+package "unzip" do
+  action :install
+end
+
+## Create ~/.fonts
+#
+diretory dotfonts do
+  group grp
+  mode "0640"
+  owner id
+  action :create
+end
+
+## Download fonts
+#
+font = "SourceCodePro_FontsOnly-1.017"
+zip_dir = ::File.join(Chef::Config[:file_cache_path], font)
+zip = "#{zip_dir}.zip"
+
+remote_file zip do
+  group grp
+  mode "0640"
+  owner id
+  source "http://sourceforge.net/projects/sourcecodepro.adobe/files/#{font}.zip"
+  action :create_if_missing
+  notifies :run, "execute[unzip #{zip}]", :immediately
+end
+
+execute "unzip #{zip}" do
+  creates zip_dir
+  cwd Chef::Config[:file_cache_path]
+  group grp
+  user id
+  action :nothing
+end
+
+execute "cp -v #{::File.join(zip_dir, "OTF")}/* #{dotfonts}" do
+  cwd zip_dir
+  group grp
+  user id
+  action :nothing
+  subscribes :run, "unzip #{zip}", :immediately
 end
