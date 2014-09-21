376a1eba98822e4167ccda548b3dd5cad76f5b79
Carlton Stedman <sageman@gmail.com>
2014-09-21 11:56:00 -0400

    xfce4 templates, some

--- recipes/xfce.rb
+++ recipes/xfce.rb
@@ -6,3 +6,37 @@
 #
 # Copyfree (F) 2014 Carlton Stedman
 #
+
+ns = node["wergstation"]
+
+# user's id, group, home
+id, grp, home = %w{ id group home }.map { |k| ns["user"][k] }
+
+cfgs = %w{ helpers.rc
+           terminal/terminalrc }.
+  map { |cfg| ".config/xfce4/#{cfg}" }
+
+## Create directory structure for xfce4 config files
+#
+cfgs.map { |cfg| cfg.rpartition("/").first }.
+  each do |dir|
+    directory ::File.join(home, dir) do
+      group grp
+      mode "0750"
+      owner "id"
+      action :create
+    end
+  end
+
+## Render xfce4 config files
+#
+cfgs.each do |cfg|
+  template ::File.join(home, cfg) do
+    group grp
+    mode "0640"
+    owner id
+    source "home/#{cfg.gsub(/\./, "dot")}.erb"
+    variables(:font => "Source Code Pro Medium 12")
+    action :create
+  end
+end
